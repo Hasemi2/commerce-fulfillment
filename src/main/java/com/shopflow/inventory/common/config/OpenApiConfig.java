2425a82ec34eadfb9aed838df3c471db64046476
Hasemi2 <h171754@gmail.com>
2026-06-19 15:29:23 +0900

    swagger 의존성 추가

---
+++ src/main/java/com/shopflow/inventory/common/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.shopflow.inventory.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI shopflowOpenApi() {
+        return new OpenAPI().info(new Info()
+            .title("Shopflow API")
+            .description("Shopflow API")
+            .version("v1"));
+    }
+}
